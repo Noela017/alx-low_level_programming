d8a911023937430470990d627f1efe9fed09231b
Noela Shisiali <shisiali017@gmail.com>
2023-02-21 13:37:40 -0800

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - check if n is greater lower or zero
+ *@n: value of n
+ * Return: Always 0 or 1.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar('+');
+	}
+	else if (n < 0)
+	{
+		return (-1);
+		_putchar('-');
+	}
+	else
+	{
+		return (0);
+		_putchar('0');
+	}
+}
