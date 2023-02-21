f1ab42c3d8f349253d019d2c8b018bbcb6ef188e
Noela Shisiali <shisiali017@gmail.com>
2023-02-21 13:54:05 -0800

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _abs - calculate value of integer
+ * @x: the integer
+ * return: value of the integer
+ *
+ */
+int _abs(int x)
+{
+	return (x * ((x > 0) - (a < 0)));
+}
