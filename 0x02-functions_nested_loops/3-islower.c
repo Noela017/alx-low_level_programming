d30fc7b342ddc583820637adba07a1fd80ead322
Noela Shisiali <shisiali017@gmail.com>
2023-02-21 13:02:46 -0800

    Task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * int _islower- check whether is lower
+ * _islower- check whether is lower
+ * @c: letter input
  * returns 1 if is true and 0 - if false
  */
 int _islower(int c)
