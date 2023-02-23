a04932041cc2f6d615df8f530cbd86b86d2c5a6c
Noela Shisiali <shisiali017@gmail.com>
2023-02-23 06:35:55 -0800

    numbersx10

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * more_numbers - print numbers ten times
+ * Return: numbers ten times
+ */
+void more_numbers(void)
+{
+	int a;
+	int c;
+
+	c = 0;
+	while (c < 10)
+	{
+		for (a = 0; a < 15; a++)
+		{
+			_putchar(a + '0');
+		}
+	c++;
+	_putchar('\n');
+	}
+}
