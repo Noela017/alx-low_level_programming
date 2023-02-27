e48fc96c8311503501b61c45fe9bb2af62a7eef4
Noela Shisiali <shisiali017@gmail.com>
2023-02-27 11:24:05 -0800

    Task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
  * _puts - function to print a string
- * @str:the string
+ * @str: the string
+ * Return: void
  */
 void _puts(char *str)
 {
-	int a;
+	int i;
 
-	for (a = 0; str[a] != '\0'; a++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(str[a]);
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
