60be086f5a19978ed5cc7fbc415ffd8f073b9af6
Noela Shisiali <shisiali017@gmail.com>
2023-03-11 11:13:01 -0800

    Another trial

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - programme to output its name
+ * progname - programme to output its name
  * @argc: num of command line arg
  * @argv: array
  * Return: 0 if success
  */
-int main(int argc, char **argv[])
+void progname(char *argv[])
 {
-	printf("%s\n", argv[argc - 1]);
-	return (0);
+	int a = 0;
+	printf("%s\n", argv[a]);
 }
