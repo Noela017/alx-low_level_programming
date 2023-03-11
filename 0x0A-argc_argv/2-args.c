6884901da98515d585fb1d5f26c58a8cc7ec9c6d
Noela Shisiali <shisiali017@gmail.com>
2023-03-11 13:26:24 -0800

    Print all arg

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - program to print all arg
+ * @argc: no of arg
+ * @argv: array
+ *
+ * Return: all args
+ */
+void main(int argc, char *argv[])
+{
+	int a;
+
+	while (a < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
