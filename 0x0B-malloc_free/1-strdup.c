11d9ff5624c1f3b75bac1bb978f44cd4b0754e3d
Noela Shisiali <shisiali017@gmail.com>
2023-03-14 08:39:02 -0700

    Task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup - func copy of the string
+ * @str: the string
+ * Return: pointer to new allo space
+ */
+char *_strdup(char *str)
+{
+	char *b;
+	int x;
+	int r;
+
+	x = 0;
+	r = 0;
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[x] != '\0')
+		i++;
+
+	b = malloc(sizeof(char) * (x + 1));
+	if (b == NULL)
+	{
+		return (NULL);
+	}
+	for (r = 0; str[r]; r++)
+	{
+		b[r] = str[r];
+	}
+	return (b);
+}
